d6976aae846fa0297f8102131b9d8c4be9997c47
BOSS <bigboss1796@hotmail.com>
2023-11-23 15:46:35 +0700

    feat:Primitive Values

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discountPercent = (25/100)*totalPrice;
+let netPay = totalPrice-discountPercent;
+console.log(netPay);
