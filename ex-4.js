4c69d1c5b4871e2fba07002abaa573d8b8780d04
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-11-23 13:24:20 +0700

    feat:EX.Primitive Values

--- ex-4.js
+++ ex-4.js
@@ -1,7 +1,5 @@
 // Exercise #4: E-commerce logic bug
 // Close Quokka before do this!
-
 let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
-
-console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+console.log((numberOfSoldProduct / numberOfProductInStock) * 100); // NaN
--- ex-7.js
+++ ex-7.js
@@ -1,7 +1,7 @@
 // Exercise #7: Concatenating String
-
 let subject = "I";
 let verb = "am";
 let object = "A Software Developer";
-
-let sentence;
+// Start coding here
+let sentence = subject + " " + verb + " " + object;
+console.log (sentence)
